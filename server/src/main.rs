db7bec5be9f170c0e97bb984e1300ba5f0648172
ckb <ckingbailey@gmail.com>
2024-09-17 21:11:17 -0700

    small refactor; rename index.html

--- server/src/main.rs
+++ server/src/main.rs
@@ -18,25 +18,18 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        println!("{request_line}");
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let content_length = contents.len();
-
-        let response =
-            format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
+    println!("{request_line}");
+    let (status_line, content_file) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
     } else {
-        println!("{request_line}");
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let content_length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response =
-            format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+    let request_body = fs::read_to_string(content_file).unwrap();
+    let content_length = request_body.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    let response =
+        format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{request_body}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
