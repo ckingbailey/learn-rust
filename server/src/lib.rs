e6bf5bc338b7730c8d6aa87b6f7d5734366dd8f8
ckb <ckingbailey@gmail.com>
2024-11-24 15:55:27 -0800

    cread thread vec inside threadpool new

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,1 +1,29 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// `new` will panic if the size is 0.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // add threads here
+        }
+
+        ThreadPool { threads }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F:FnOnce() + Send + 'static
+    {}
+}
