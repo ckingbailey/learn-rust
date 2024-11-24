780a22c7d305702af37af99639d9073e436e0752
ckb <ckingbailey@gmail.com>
2024-11-24 15:56:15 -0800

    fix execute method call

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        pool::execute(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
