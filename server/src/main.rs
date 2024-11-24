a131e99c130e1044df4a60ca929d7497f6fe3948
ckb <ckingbailey@gmail.com>
2024-11-24 15:19:33 -0800

    slow route

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpStream, TcpListener},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -21,10 +23,13 @@ fn handle_connection(mut stream: TcpStream) {
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     println!("{request_line}");
-    let (status_line, content_file) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, content_file) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let request_body = fs::read_to_string(content_file).unwrap();
