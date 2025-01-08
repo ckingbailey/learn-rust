c2fa43b4f8aa9b28cc7c777d4849d7fe0cc40c4a
ckb <ckingbailey@gmail.com>
2025-01-07 20:56:28 -0800

    wrap thread in option

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,20 +5,20 @@ use std::{
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            thread: thread::spawn(move || loop {
+            thread: Some(thread::spawn(move || loop {
                 let job = receiver.lock().unwrap().recv().unwrap();
 
                 println!("Worker {id} got a job; executing.");
 
                 job();
-            })
+            }))
         }
     }
 }
@@ -60,5 +60,17 @@ impl ThreadPool {
         let job = Box::new(f);
 
         self.sender.send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
