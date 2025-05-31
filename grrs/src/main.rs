82942ba6f886e250a73c6f8f1db04ae804e09177
ckb <ckingbailey@gmail.com>
2025-05-31 10:22:21 -0700

    read entire file content to String

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path path given");
+    let file_path = std::env::args().nth(2).expect("No path path given");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path);
+    println!("pattern: {:?}, path: {:?}", pattern, file_path);
+
+    let contents: String = std::fs::read_to_string(file_path).expect("Could not read file");
+
+    println!("Read {} chars from file", contents.len());
 }
