f4342d00637931d788976c2d917933e016136160
ckb <ckingbailey@gmail.com>
2025-05-31 13:14:50 -0700

    use clap cli

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,13 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    pattern: String,
+    file_path: String
+}
+
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let file_path = std::env::args().nth(2).expect("No path path given");
+    let cli = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", pattern, file_path);
+    println!("pattern: {:?}, path: {:?}", cli.pattern, cli.file_path);
 
-    let path_slc = &file_path[..];
-    let contents = match std::fs::read_to_string(path_slc) {
+    // let path_slc = &file_path[..];
+    let contents = match std::fs::read_to_string(&cli.file_path) {
         Ok(contents) => contents,
-        Err(_) => panic!("Could not read file {}", path_slc)
+        Err(_) => panic!("Could not read file {}", cli.file_path)
     };
 
     println!("Read {} chars from file", contents.len());
